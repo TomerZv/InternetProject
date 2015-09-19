89e354625252eedb3f328a62eb06a6bee3c1cf29
TomerZv <tomerzv@gmail.com>
2015-09-19 23:57:25 +0300

    Revert "add identity functions"
    This reverts commit 550638a02ec5d8d09a80874d5971e8822daf2899.

--- ShauliBlog/App_Start/IdentityConfig.cs
+++ ShauliBlog/App_Start/IdentityConfig.cs
@@ -23,6 +23,7 @@ namespace ShauliBlog
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
                 AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
             };
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
--- ShauliBlog/Models/AccountViewModels.cs
+++ ShauliBlog/Models/AccountViewModels.cs
@@ -2,37 +2,18 @@
 
 namespace ShauliBlog.Models
 {
-    public class LoginViewModel
+    public class ExternalLoginConfirmationViewModel
     {
         [Required]
-        [Display(Name = "User Name")]
-        public string UserName { get; set; }
-
-        [Required]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
-
-        [Display(Name = "Remember me?")]
-        public bool RememberMe { get; set; }
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
     }
 
-    public class RegisterViewModel
+    public class ExternalLoginListViewModel
     {
-        [Required]
-        [Display(Name = "User Name")]
-        public string UserName { get; set; }
-
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
+        public string Action { get; set; }
+        public string ReturnUrl { get; set; }
     }
 
     public class ManageUserViewModel
@@ -54,10 +35,46 @@ namespace ShauliBlog.Models
         public string ConfirmPassword { get; set; }
     }
 
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
     public class ResetPasswordViewModel
     {
         [Required]
-        [Display(Name = "User Name")]
+        [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
@@ -74,4 +91,11 @@ namespace ShauliBlog.Models
         public string Code { get; set; }
     }
 
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
 }
