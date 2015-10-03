791f09d337e8222767a7a041aebff3d2160e51db
urielb92 <urielb92@gmail.com>
2015-10-03 18:07:05 +0300

    fixing layout

--- ShauliBlog/Scripts/Shared/layout.js
+++ ShauliBlog/Scripts/Shared/layout.js
@@ -5,4 +5,18 @@
     ctx.fillStyle = "red";
     ctx.textAlign = "left";
     ctx.fillText("Eyze Amatzya? Amatzya shelanu?", 0, canvas.height / 2);
+
+    $.ajax({
+        url: '/statistics/GetPopularComments',
+        type: 'POST',
+        contentType: 'application/json'
+    }).done(function (posts) {
+
+        jQuery.each(posts, function (index, currPost) {
+            var newLine = '<li><a href="/#post ' + currPost.Id + '" >' + currPost.Post + '</a></li>';
+            $('#popularList').append(newLine);
+        });
+
+    });
+
 });
