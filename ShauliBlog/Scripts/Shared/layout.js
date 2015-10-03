9dd7871772eac5f3eebb6634cbb3f3f3faac4797
urielb92 <urielb92@gmail.com>
2015-10-03 18:05:06 +0300

    commiting layout

---
+++ ShauliBlog/Scripts/Shared/layout.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    ctx.font = "12px Comic Sans MS";
+    ctx.fillStyle = "red";
+    ctx.textAlign = "left";
+    ctx.fillText("Eyze Amatzya? Amatzya shelanu?", 0, canvas.height / 2);
+});
